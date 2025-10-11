9e1859c8a0821b81b7afb90e8926838bb2b9ed9e
Celes Renata <celes@frameshift.net>
2025-10-11 09:27:52 -0700

    Rename touch-config.qml to shell.qml for proper quickshell structure

--- packages/hyte-touch-interface.nix
+++ packages/hyte-touch-interface.nix
@@ -1,9 +1,14 @@
 { pkgs, quickshell }:
 
 let
-  touchConfig = ../config/quickshell/touch-config.qml;
+  touchConfigDir = pkgs.runCommand "touch-config" {} ''
+    mkdir -p $out/quickshell/touch
+    cp ${../config/quickshell/shell.qml} $out/quickshell/touch/shell.qml
+    cp -r ${../config/quickshell/widgets} $out/quickshell/touch/ || true
+  '';
 in
 pkgs.writeShellScriptBin "hyte-touch-interface" ''
+  export XDG_CONFIG_HOME=${touchConfigDir}
   export QML_IMPORT_PATH="${quickshell}/lib/qt-6/qml"
-  exec ${quickshell}/bin/quickshell -c ${touchConfig}
+  exec ${quickshell}/bin/quickshell -c touch
 ''
