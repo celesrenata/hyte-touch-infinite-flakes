1f31664ca955e818caec22d3e7add0dd60a53337
Celes Renata <celes@frameshift.net>
2025-10-11 09:22:31 -0700

    Fix hyte-touch-interface path reference

--- packages/hyte-touch-interface.nix
+++ packages/hyte-touch-interface.nix
@@ -1,6 +1,9 @@
 { pkgs, quickshell }:
 
+let
+  touchConfig = ../config/quickshell/touch-config.qml;
+in
 pkgs.writeShellScriptBin "hyte-touch-interface" ''
   export QML_IMPORT_PATH="${quickshell}/lib/qt-6/qml"
-  exec ${quickshell}/bin/quickshell -c ${../config/quickshell/touch-config.qml}
+  exec ${quickshell}/bin/quickshell -c ${touchConfig}
 ''
